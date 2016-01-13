4222805cfc9e1e0fc77892ddbe7d41837586e82e
Steve Buckley <buckleys78@gmail.com>
2016-01-13 00:54:11 -0600

    Bin and CanRack classes written.

---
+++ Bin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVendingMachine {
+    class Bin {
+        private int maxCapacity = 3;
+
+        public Bin(string flavor) {
+            Flavor = flavor;
+        }
+
+        public bool IsEmpty {
+            get {
+                return Quantity == 0;
+            }
+        }
+
+        public bool IsFull {
+            get {
+                return Quantity == maxCapacity;
+            }
+        }
+
+        public string Flavor { get; set; }
+        public int Quantity { get; set; }
+
+        // methods
+        public void AddCan() {
+            if (!IsFull) {
+                Quantity++;
+            }
+        }
+
+        public void EmptyBin() {
+            Quantity = 0;
+        }
+
+        public void FillBin() {
+            Quantity = maxCapacity;
+        }
+
+        public void RemoveCan() {
+            if (!IsEmpty) {
+                Quantity--;
+            }
+        }
+    }
+}
---
+++ CanRack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVendingMachine {
+    class CanRack {
+        // constructor(s)
+        public CanRack() {
+            ConfigureTheCanRack();
+            FillTheCanRack();
+        }
+
+        // properties
+        Dictionary<string, Bin> Bins { get; set; }
+
+        // methods
+        public void AddACanOf(string flavorName) {
+            Bin bin;
+            if (Bins.TryGetValue(flavorName, out bin)) {
+                bin.AddCan();
+            }
+        }
+
+        private void ConfigureTheCanRack() {
+            Dictionary<string, Bin> Bins = new Dictionary<string, Bin>();
+            Bins.Add("Regular", new Bin("Regular"));
+            Bins.Add("Orange", new Bin("Orange"));
+            Bins.Add("Lemon", new Bin("Lemon"));
+        }
+
+        public void EmptyTheCanRackOf(string flavorName) {
+            Bin bin;
+            if (Bins.TryGetValue(flavorName, out bin)) {
+                bin.EmptyBin();
+            }
+        }
+
+        public void FillTheCanRack() {
+            foreach (var flavorName in Bins.Keys) {
+                Bins[flavorName].FillBin();
+            }
+        }
+
+        public bool IsEmpty(string flavorName) {
+            Bin bin;
+            bool isEmpty = true;
+            if (Bins.TryGetValue(flavorName, out bin)) {
+                isEmpty = bin.IsEmpty;
+            }
+            return isEmpty;
+        }
+
+        public bool IsFull(string flavorName) {
+            Bin bin;
+            bool isFull = false;
+            if (Bins.TryGetValue(flavorName, out bin)) {
+                isFull = bin.IsFull;
+            }
+            return isFull;
+        }
+
+        public void RemoveACanOf(string flavorName) {
+            Bin bin;
+            if (Bins.TryGetValue(flavorName, out bin)) {
+                bin.RemoveCan();
+            }
+        }
+
+    }
+
+}
