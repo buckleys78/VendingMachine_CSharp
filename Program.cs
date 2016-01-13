3c627f7ef391b5a19ce477d9b112abc97e619536
Steve Buckley <buckleys78@gmail.com>
2016-01-12 23:41:54 -0600

    Added PurchasePrice class and implemented.

--- Program.cs
+++ Program.cs
@@ -6,18 +6,18 @@
 namespace SimpleVendingMachine {
     class Program {
         static void Main(string[] args) {
-            int costOfOneSoda = 35;
+            PurchasePrice priceOfOneSoda = new PurchasePrice(35);
             int totalAmountInserted = 0;
 
             WriteLine("Welcome to the .NET C# Soda Vending Machine");
-            Write($"Please insert {costOfOneSoda} cents: ");
+            Write($"Please insert {priceOfOneSoda.Price} cents: ");
 
-            while (totalAmountInserted < costOfOneSoda) {
+            while (totalAmountInserted < priceOfOneSoda.Price) {
                 string userResponse = ReadLine();
                 int amountInsertedThisTime = 0;
                 int.TryParse(userResponse, out amountInsertedThisTime);
                 totalAmountInserted += amountInsertedThisTime;
-                int amountShort = costOfOneSoda - totalAmountInserted;
+                int amountShort = priceOfOneSoda.Price - totalAmountInserted;
                 if (amountShort > 0) {
                     Write($"Please insert at least {amountShort} cents more: ");
                 }
---
+++ PurchasePrice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVendingMachine {
+    class PurchasePrice {
+        public PurchasePrice() {
+            Price = 0;
+        }
+
+        public PurchasePrice(int initialPrice) {
+            Price = initialPrice;
+        }
+
+        public int Price { get; set; }
+
+    }
+}
